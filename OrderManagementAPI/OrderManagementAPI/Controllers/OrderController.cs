6e777fc2c3206a260f13423e6d581da7a47e5197
Jya <Jya@DESKTOP-COP40JO>
2025-03-21 18:42:04 +0700

    HoangNH : API Completed

---
+++ OrderManagementAPI/OrderManagementAPI/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagementAPI.Application.UseCases.OrderUseCases.DTOs;
+using OrderManagementAPI.Application.UseCases.OrderUseCases.Repositories;
+
+namespace OrderManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrderController(IOrderRepository orderRepository) : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository = orderRepository;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var orders = await _orderRepository.GetAllOrderAsync();
+            return Ok(orders);
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<IActionResult> GetOrderById(int orderId)
+        {
+            var order = await _orderRepository.GetOrderByIdAsync(orderId);
+            if (order == null)
+                return NotFound();
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequest request)
+        {
+            var result = await _orderRepository.CreateNewOrderAsync(request);
+            return CreatedAtAction(nameof(GetOrderById), new { status = result }, result);
+        }
+
+        [HttpPut("{orderId}")]
+        public async Task<IActionResult> UpdateOrder(int orderId, [FromBody] UpdateOrderRequest request)
+        {
+            var result = await _orderRepository.UpdateOrderAsync(orderId, request);
+            return Ok(result);
+        }
+
+        [HttpDelete("{orderId}")]
+        public async Task<IActionResult> DeleteOrder(int orderId)
+        {
+            var result = await _orderRepository.DeleteOrderAsync(orderId);
+            return Ok(result);
+        }
+    }
+}
---
+++ OrderManagementAPI/OrderManagementAPI/Controllers/OrderDetailController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderManagementAPI.Application.UseCases.OrderDetailUseCases.DTOs;
+using OrderManagementAPI.Application.UseCases.OrderDetailUseCases.Repositories;
+
+namespace OrderManagementAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class OrderDetailController : ControllerBase
+    {
+        private readonly IOrderDetailRepository _orderDetailRepository;
+        public OrderDetailController(IOrderDetailRepository orderDetailRepository)
+        {
+            _orderDetailRepository = orderDetailRepository;
+        }
+
+        [HttpPost("orders/{orderId}/orderdetails")]
+        public async Task<IActionResult> CreateOrderDetail(int orderId, [FromBody] List<CreateOrderDetailRequest> request)
+        {
+            var result = await _orderDetailRepository.CreateOrderDetailAsync(orderId, request);
+            return Ok(result);
+        }
+
+        [HttpGet("orders/{orderId}/orderdetails")]
+        public async Task<IActionResult> GetOrderDetail(int orderId)
+        {
+            var orderDetails = await _orderDetailRepository.GetOrderDetailAsync(orderId);
+            return Ok(orderDetails);
+        }
+
+        [HttpDelete("orders/{orderId}/orderdetails/{orderDetailId}")]
+        public async Task<IActionResult> DeleteOrderDetail(int orderId, int orderDetailId)
+        {
+            var result = await _orderDetailRepository.DeleteOrderDetailFromOrderAsync(orderId, orderDetailId);
+            return Ok(result);
+        }
+    }
+}
